f936fc4c52afb3adf330de21603dd54749257e3d
Justin Hager <coachjustinhager@gmail.com>
2020-11-30 11:53:36 -0800

    add GET / smoke test

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,5 +1,10 @@
 const app = require('../src/app')
 
 describe('App', () => {
-    
+    it(`GET / responds with 200`, () => {
+        return supertest(app)
+            .get('/')
+            .set('Authorization', 'Bearer ' + process.env.API_TOKEN)
+            .expect(200)
+    })
 })
